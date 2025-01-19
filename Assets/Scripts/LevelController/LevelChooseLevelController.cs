de8c7545623e0361d04061bf4c48dc4fb6c332df
YXHXianYu <2943003@qq.com>
2025-01-19 02:35:25 +0000

    empty message

--- Assets/Scripts/LevelController/LevelChooseLevelController.cs
+++ Assets/Scripts/LevelController/LevelChooseLevelController.cs
@@ -1,6 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+
+[System.Serializable]
+public class RayReceiverAndLevel {
+    public RayReceiverMapElement rayReceiver;
+    public string levelName;
+}
 
 public class LevelChooseLevelController : BaseLevelController {
+
+    [Header("Level Choose")]
+    public string levelChooseSceneName = "Level00_ChooseLevel";
+    public List<RayReceiverAndLevel> levelList = new List<RayReceiverAndLevel>();
+
+    protected override void Update() {
+        base.Update();
+
+        foreach (var rcvAndLevel in levelList) {
+            var rayReceiver = rcvAndLevel.rayReceiver;
+            var levelName = rcvAndLevel.levelName;
+            if (rayReceiver.isRayReceived) {
+
+                SceneManager.UnloadSceneAsync(levelChooseSceneName).completed += (asyncOperation) => {
+                    SceneManager.LoadSceneAsync(levelName, LoadSceneMode.Additive);
+                };
+                return;
+            }
+        }
+    }
+
 }
