f4e442758a9faa0c9615163006e9b56ca185556f
YXHXianYu <2943003@qq.com>
2025-01-27 14:11:04 +0800

    fix: fix ui squeezed on low aspect ratio devices

--- Assets/Scripts/Camera/CameraAspectRatioClamper.cs
+++ Assets/Scripts/Camera/CameraAspectRatioClamper.cs
@@ -3,19 +3,18 @@ using Cinemachine;
 
 public class CameraAspectRatioClamper : MonoBehaviour {
     public CinemachineVirtualCamera virtualCamera;
+    public float initialOrthographicSize = 5.3f;
     public float minAspectRatio = 1.6f; // E.g. 16:10
+    public float ratioScaleCoefficient = 1.0f;
 
     private void Update() {
         var currentAspect = (float)Screen.width / Screen.height;
+        var ratio = 1.0f;
+        var lens = virtualCamera.m_Lens;
         if (currentAspect < minAspectRatio) {
-            var ratio = minAspectRatio / currentAspect;
-            var lens = virtualCamera.m_Lens;
-            lens.Orthographic = true;
-            lens.OrthographicSize *= ratio; 
-            virtualCamera.m_Lens = lens;
-            Debug.Log("Current aspect ratio: " + currentAspect + "; Clamped to: " + minAspectRatio);
-        } else {
-            Debug.Log("Current aspect ratio: " + currentAspect + "; No clamping needed.");
+            ratio = minAspectRatio / currentAspect;
         }
+        lens.OrthographicSize = initialOrthographicSize * Mathf.Pow(ratio, ratioScaleCoefficient); 
+        virtualCamera.m_Lens = lens;
     }
 }
